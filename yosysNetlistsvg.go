828852756ea258cad355821adff7f8d93a141355
Lars Erik Skraastad <larserikskraastad@gmail.com>
2019-02-21 14:01:12 +0100

    added a cleanup file

--- yosysNetlistsvg.go
+++ yosysNetlistsvg.go
@@ -2,45 +2,24 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os/exec"
 )
 
 func executeYosys(path string) {
 	app := "yosys/yosys"
-	// app := "cat"
-	// arg0 := []string{"-p", "\"prep -auto-top; aigmap; write_json larstestOutAigmap.json\"", path}
 	arg0 := []string{"-p", "prep -auto-top; aigmap; write_json larstestOutAigmap.json", path}
-	// app := "ls"
-	// arg0 := "-la"
-	// arg1 := "\"prep -auto-top; aigmap; write_json larstestOutAigmap.json\""
-	// arg2 := path
-	// app := "tr"
-	// arg1 := "a-z"
-	// arg2 := "A-Z"
 	yosysCmd := exec.Command(app, arg0...) //, arg1, arg2)
-	fmt.Println(yosysCmd)
 
-	// out, err := yosysCmd.Output()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Output() failed with '%s'\n", err)
-	// }
-
-	// print(string(out))
-
-	// -p "prep -auto-top; aigmap; write_json larstestOutAigmap.json" ../yosys/tests/simple/larserik/counter.v
-
-	yosysOut, _ := yosysCmd.StdoutPipe() //Making a pipe we can reseive responses
-
-	err := yosysCmd.Start()
+	// yosysOut, _ := yosysCmd.StdoutPipe() //Making a pipe we can reseive responses
+	err := yosysCmd.Run()
 	if err != nil {
 		log.Fatalf("cmd.Start() failed with '%s'\n", err)
 	}
-	yosysBytes, _ := ioutil.ReadAll(yosysOut)
+	// yosysBytes, _ := ioutil.ReadAll(yosysOut)
 	// fmt.Println("> Yosys Svarer")
-	fmt.Println(string(yosysBytes))
-	yosysCmd.Wait()
+	// fmt.Println(string(yosysBytes))
+	// yosysCmd.Wait()
 }
 
 func executeNetlistsvg() {
@@ -70,6 +49,7 @@ func main() {
 	executeYosys("yosys/tests/simple/larserik/counter.v")
 	executeNetlistsvg()
 	displaySvg()
+	fmt.Println("Done")
 
 }
 
